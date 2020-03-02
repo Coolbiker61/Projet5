51525fcadce61aa2fc7fa4080232b0d02f082b3b
Coolbiker61 <coolbiker61@gmail.com>
2020-03-02 15:59:54 +0100

    ajout de commentaire sur confirm et ajout de la commande de suppression du panier ainsii qu'une taille max pour les images

--- site/confirm.js
+++ site/confirm.js
@@ -1,15 +1,19 @@
-
+/* affiche le retour de la commande */
 const afficheCommande = () => {
 	let html = "";
 	let contenuCoordonne = "";
 	var commande
 	prixTotal = 0;
+	/* Verifie l'existance de retourCommande dans le localStorage et recupére son contenu puis supprime le panier*/
 	if (localStorage.getItem("retourCommande")) {
 		commande = JSON.parse(localStorage.getItem("retourCommande"));
+		localStorage.removeItem("panier");
 	} else {
 		return;
 	}
+	/* ajoute l'id de la commande dans la balise html qui a l'id commande-id */
 	document.getElementById("commande-id").innerHTML = commande.orderId;
+	/* recupere les élements de contact du retour de commande et les ajoutent à la page html */
 	contenuCoordonne += "<span class=\"titre-contact\">Vos coordonnées : </span>";
 	contenuCoordonne += "<div>Nom : "+commande.contact.lastName+"</div>";
 	contenuCoordonne += "<div>Prénom : "+commande.contact.firstName+"</div>";
@@ -17,6 +21,7 @@ const afficheCommande = () => {
 	contenuCoordonne += "<div>Ville : "+commande.contact.city+"</div>";
 	contenuCoordonne += "<div>Email : "+commande.contact.email+"</div>";
 	document.getElementById("commande-coordonnées").innerHTML = contenuCoordonne;
+	/* pour chaque article de la commande, les balises html et les details de l'article sont ajouté a la variable html */
 	for (let article of commande.products) {
 		let image = article.imageUrl;
 		let nom = article.name;
@@ -26,16 +31,17 @@ const afficheCommande = () => {
 		let description = article.description;
 		let id = article._id;
 		
-		html += "<div class=\"objet\"  id=\""+id+"\">";
-		html += "<img class=\"img-produit\" src=\""+image+"\"><br>";
+		html += "<div class=\"objet panier\"  id=\""+id+"\">";
+		html += "<img class=\"img-panier\" src=\""+image+"\"><br>";
 		html += "<div class=\"details-panier\"><h3>"+nom+"</h3>"
 		html += "<p>"+description+"</p>";
 		html += "<div class=\"prix\">Prix : "+prix+"€</div>";
 		html += "</div></div>";
 
 	}
-		
+	/* ajout du prix total a la variable html */
 	html += "<div class=\"total\" id=\"total\">Total de la commande : "+(prixTotal/100)+"€";
+	/* insertion du contenu de la variable html dans le DOM */
 	document.getElementById("commande-articles").innerHTML = html;
 } 
 
